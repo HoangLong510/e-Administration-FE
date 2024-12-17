deb1e03dab52a7b6b8b6d5959e465f3de41bed5a
Long <hoanglongnguyen0510@gmail.com>
2024-12-17 21:24:37 +0700

    [Long] Add user management functions including viewing, adding, and disabling users for administrators

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,11 @@
 import DefaultLayout from "~/layouts/DefaultLayout"
+import CreateUser from "~/pages/management/user/CreateUser"
 import User from "~/pages/management/user/User"
 
 
 const adminRoutes = [
-    { path: '/management/user', component: User, layout: DefaultLayout }
+    { path: '/management/user', component: User, layout: DefaultLayout },
+    { path: '/management/user/create', component: CreateUser, layout: DefaultLayout }
 ]
 
 export default adminRoutes
---
+++ src/utils/regex.js
@@ -0,0 +1,24 @@
+const regex = {
+    fullName: {
+        pattern: /^(?!\s)[a-zA-Z\u0080-\uFFFF\s]{2,50}(?<!\s)$/u,
+        message: "Full name must be between 2 and 50 characters and not contain any special characters"
+    },
+    username: {
+        pattern: /^[a-z][a-z0-9_]{4,19}$/,
+        message: "Username must be between 5 and 20 characters. Must start with a lowercase letter"
+    },
+    phone: {
+        pattern: /^(0|\+84)(\s|\.)?((3[2-9])|(5[26-9])|(7[0|6-9])|(8[0-9])|(9[0-9]))(\d){7}$/,
+        message: "Phone number must be in the format +84909123123 or 0909123123"
+    },
+    password: {
+        pattern: /^.{6,30}$/,
+        message: "Password must be between 6 and 30 characters"
+    },
+    email: {
+        pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: "Email address is not valid"
+    }
+}
+
+export default regex
