a4538ea2d25464f8b0b961361aa0a523f95892dc
nhlinh0302 <nhatlinh3b122@gmail.com>
2024-12-17 21:21:33 +0700

    [Linh] Add classroom management function for administrators

---
+++ src/pages/management/Class/service.js
@@ -0,0 +1,103 @@
+import axios from "~/axios";
+
+export const getAllClasses = async ({ search = "", page = 1, pageSize = 10 } = {}) => {
+  try {
+    const res = await axios.get('/management/class', {
+      params: {
+        search,
+        page,
+        pageSize
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    return {
+      success: false,
+      message: "Server is having problems, please try again later"
+    };
+  }
+};
+
+
+export const getClassById = async (id) => {
+  try {
+    const res = await axios.get(`/management/class/${id}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    return {
+      success: false,
+      message: "Server is having problems, please try again later"
+    };
+  }
+};
+
+export const addClass = async (newClass) => {
+  try {
+    const res = await axios.post('/management/class', newClass, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    return {
+      success: false,
+      message: "Server is having problems, please try again later"
+    };
+  }
+};
+
+// Xóa lớp học
+export const deleteClass = async (id) => {
+  try {
+    const res = await axios.delete(`/management/class/${id}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    return {
+      success: false,
+      message: "Server is having problems, please try again later"
+    };
+  }
+};
+
+export const updateClass = async (id, updatedClass) => {
+  try {
+    const res = await axios.put(`/management/class/${id}`, updatedClass, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return res.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    return {
+      success: false,
+      message: "Server is having problems, please try again later"
+    };
+  }
+};
