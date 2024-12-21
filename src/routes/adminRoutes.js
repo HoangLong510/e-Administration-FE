cf225afbb61f7dfa8ec66029832168c872441b05
aq0811 <nguyenanhquan081120@gmail.com>
2024-12-21 23:03:38 +0700

    [quan] lab management

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,11 @@
 import DefaultLayout from "~/layouts/DefaultLayout"
+import Lab from "~/pages/management/lab/Lab"
 import User from "~/pages/management/user/User"
 
 
 const adminRoutes = [
-    { path: '/management/user', component: User, layout: DefaultLayout }
+    { path: '/management/user', component: User, layout: DefaultLayout },
+    { path: '/management/lab', component: Lab, layout: DefaultLayout }
 ]
 
 export default adminRoutes
