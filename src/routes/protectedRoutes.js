5cc6398b13880f0367be0720fcfff0db69994f72
hoanggiahuy <huypg7645@gmail.com>
2024-12-27 23:53:12 +0700

    huy-schedule

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -1,10 +1,23 @@
 import Home from "~/pages/home/Home"
+import Schedule from "~/pages/schedule/Schedule"
 import DefaultLayout from "~/layouts/DefaultLayout"
 
 const protectedRoutes = [
     {
         path: '/',
         component: Home,
+        layout: DefaultLayout,
+        roles: [
+            "Admin",
+            "Instructor",
+            "HOD",
+            "TechnicalStaff",
+            "Student"
+        ]
+    },
+    {
+        path: '/schedule',
+        component: Schedule,
         layout: DefaultLayout,
         roles: [
             "Admin",
