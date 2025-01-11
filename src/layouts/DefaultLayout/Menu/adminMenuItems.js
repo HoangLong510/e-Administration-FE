3b12055e1205587bbc196f1d844e98a98a6f8d30
nhlinh0302 <nhatlinh3b122@gmail.com>
2025-01-12 03:07:39 +0700

    [Linh]-ReportDetails-fix

--- src/layouts/DefaultLayout/Menu/adminMenuItems.js
+++ src/layouts/DefaultLayout/Menu/adminMenuItems.js
@@ -1,5 +1,4 @@
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts'
-import FeedbackIcon from '@mui/icons-material/Feedback'
 import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded'
 import RoomPreferencesRoundedIcon from '@mui/icons-material/RoomPreferencesRounded'
 import ClassRoundedIcon from '@mui/icons-material/ClassRounded';
@@ -12,11 +11,6 @@ const adminMenuItems = [
         label: "Users",
         href: `/management/user`,
         icon: ManageAccountsIcon
-    },
-    {
-        label: "Feedbacks",
-        href: `/management/feedback`,
-        icon: FeedbackIcon
     },
     {
         label: "Divices",
